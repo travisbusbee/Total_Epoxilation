6d3b30bcb9557451c8a886cdb525f14f289f7980
jminardi <jack@minardi.org>
2014-08-26 21:45:24 -0400

    works on mama

---
+++ epoxy_AUTOMATION_SETUP.py
@@ -0,0 +1,121 @@
+from mecode import G
+import numpy as np
+from aerotech_automator import AerotechAutomator
+
+
+#Location of written GCode file generated from this script
+outfile = r"C:\Users\Lewis Group\Documents\GitHub\aerotech_automation\cell_printing_out.pgm"
+
+#List of axes used for printing - comment out the axes not being used
+AXES_USED = ['A',
+            'B',
+            #'C', 
+            #'D'
+            ]
+
+#Defining positions of axes
+AXES_DATA = {
+    'A': {
+        'number': 4,
+        'alignment_location': (586.075, 367.82),
+    },
+    'B': {
+        'number': 5,
+        'alignment_location': (482.075, 367.82),
+    },
+    'C': {
+        'number': 6,
+        'alignment_location': (378.075, 367.82),
+    },
+    'D': {
+        'number': 7,
+        'alignment_location': (299.075, 367.82),
+    },
+}
+
+#Defining substrate location and profilometry mesh size
+SUBSTRATES = {
+    'slide1': {
+        'origin': (180,109),
+        'size': 'auto',
+        'profile': True,
+        'profile-spacing': (20,45),
+    },
+}
+#Defining profilometry parameters
+automator = AerotechAutomator(
+    calfile_path=r'C:\Users\Lewis Group\Desktop\Calibration\CAL_output.cal',
+    axes=AXES_USED,
+    axes_data = AXES_DATA,
+    substrates = SUBSTRATES,
+)
+
+#Defining mecode parameters
+g = G(
+    direct_write=True,
+    outfile=outfile,
+    header=None,
+    footer=None,
+    print_lines=False,
+    )
+
+
+def setup(ref, move_to_ref = False):
+    automator.setup()
+    automator.automate()
+    automator.save_state(r"C:\Users\Lewis Group\Desktop\Calibration\alignment_data.txt")
+    automator.teardown()
+    g.direct_write = True
+    #g.set_cal_file(r'C:\Users\Lewis Group\Desktop\Calibration\CAL_output.cal')
+    g.direct_write = False
+    if move_to_ref is True:
+        g.direct_write = True
+        g.abs_move(x=automator.substrate_origins['slide1'][ref][0], y=automator.substrate_origins['slide1'][ref][1] + 5)
+        g.abs_move(**{ref:(automator.substrate_origins['slide1'][ref][2]+ 0.2)})
+        g.direct_write = False
+        
+        
+
+################ Full Setup Run
+reference_nozzle = 'A' 
+
+
+setup(ref = reference_nozzle, move_to_ref = True)
+
+#########
+
+################Rezero some nozzles but use the rest of the old info ###
+#automator.setup()
+#automator.load_state(r"C:\Users\Lewis Group\Desktop\Calibration\alignment_data.txt")
+#automator.zero_nozzle('D')
+#automator.save_state(r"C:\Users\Lewis Group\Desktop\Calibration\alignment_data.txt")
+#g.direct_write = True
+#g.abs_move(x=automator.substrate_origins['slide1'][reference_nozzle][0], y=automator.substrate_origins['slide1'][reference_nozzle][1] + 5)
+#g.abs_move(**{reference_nozzle:(automator.substrate_origins['slide1'][reference_nozzle][2]+ 0.2)})
+#g.direct_write = False
+ 
+#automator.setup()
+#automator.load_state(r"C:\Users\Lewis Group\Desktop\Calibration\alignment_data.txt")
+#automator.rezero_nozzles(['A','B','C'], alignment_path=r"C:\Users\Lewis Group\Desktop\Calibration\alignment_data.txt", cal_file=True)
+
+## Reset zeros ############
+
+
+#automator.substrate_origins['slide1']['B'][2]
+
+
+
+        #automator.substrate_origins['slide1'][reference_nozzle][2]-0.01
+        
+
+g.write("POSOFFSET CLEAR X Y U A B C D")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
--- gcode_modifier_preremove.py
+++ gcode_modifier_preremove.py
@@ -1,27 +1,51 @@
 from mecode import G
+from aerotech_automation import AerotechAutomator
+automator = AerotechAutomator()
 g = G(print_lines = True)
+automator.load_state(r"C:\Users\Lewis Group\Desktop\Calibration\alignment_data.txt")
+
+zA  = zB = zC = zD =0
+zA = automator.substrate_origins['slide1']['A'][2]
+zB = automator.substrate_origins['slide1']['B'][2]
+x_offset = (automator.home_positions['B'][0] - automator.home_positions['A'][0])
+y_offset = (automator.home_positions['B'][1] - automator.home_positions['A'][1])
+
 original_file = r'/Users/busbees/Desktop/gcode_test.gcode'
 modified_file = r'/Users/busbees/Desktop/text_test_mod.txt'
-x_offset = 104
-y_offset = 1
-silver_feed = 4
-matrix_feed = 12
 
-f1 = open(r'C:\Users\tbusbee\Documents\Voxel8 Sync\Travis\3D epoxy code\3D epoxy assembly - Matrix_epoxy3d-1.amf.gcode', 'r')
-f2 = open(r'C:\Users\tbusbee\Documents\Voxel8 Sync\Travis\3D epoxy code\3D epoxy_test_mod.txt', 'w')
-f3 = open(r'C:\Users\tbusbee\Documents\Voxel8 Sync\Travis\3D epoxy code\gcode_test_removed.txt', 'w')
+silver_feed = 4
+matrix_feed = 40
+com_port = 4
+silver_pressure = 12
+matrix_pressure = 7
+
+f1 = open(r'C:\Users\Lewis Group\Documents\GitHub\Total_Epoxilation\3D epoxy assembly - Matrix_epoxy3d-1.amf.gcode', 'r')
+f2 = open(r'C:\Users\Lewis Group\Documents\GitHub\Total_Epoxilation\3D epoxy_test_mod.pgm', 'w')
+f3 = open(r'C:\Users\Lewis Group\Documents\GitHub\Total_Epoxilation\gcode_test_removed.txt', 'w')
+header = open(r'C:\Users\Lewis Group\Documents\GitHub\Total_Epoxilation\epoxy_header.txt', 'r')
+footer = open(r'C:\Users\Lewis Group\Documents\GitHub\Total_Epoxilation\epoxy_footer.txt', 'r')
 tool_status = 1
 z_axis = 'A'
+Aaxis_multiplier = 0
+Baxis_multiplier = 1
 valve = 0
 ident = 0
 check = 0
 E_in = False
+skirt = False
 
+
+
+
+### Parse and remove bullshit like retraction and write new file###
 start_write = False
 for line in f1:
     if "set temperature" in line:
         ident = 1
-    if "disable fan" in line:
+    if skirt is False:
+        if "skirt" in line:
+            ident = 1
+    if "fan" in line:
         ident = 1
     if "retract" in line:
         ident = 1
@@ -42,17 +66,36 @@ for line in f1:
 
 f1.close()
 f3.close()
-f3 = open(r'C:\Users\tbusbee\Documents\Voxel8 Sync\Travis\gcode_test_removed.txt', 'r')
+f3 = open(r'C:\Users\Lewis Group\Documents\GitHub\Total_Epoxilation\gcode_test_removed.txt', 'r')
+
+### Write header to file ####
+stuff = header.readlines()
+f2.writelines(stuff)  
+f2.write('Call setPress P{} Q{}\n'.format(com_port, matrix_pressure))
+f2.write('Call togglePress P{}\n'.format(com_port))
+
+### set z home ###########
+f2.write('POSOFFSET CLEAR A B C D\n')
+f2.write('G1 F25\n')
+f2.write('G90\n')
+f2.write('G1 A-2 B-2 C-2 D-2\n')
+f2.write('G92 A{} B{} C{} D{}\n'.format(-zA - 2, -zB - 2, -zC - 2, -zD-2))
+f2.write('G1 F{}\n'.format(matrix_feed))
+#################################
 
 
-
-
-
+### parse and edit file that has bullshit removed ###
 for line in f3:
+    if "Z" in line:
+        back_end = line.split(r'Z', 1)[1]
+        number = float(back_end.split(r' ', 1)[0])
+        temp_line = 'G1 {}{} {}{}'.format('A', number +10*Aaxis_multiplier, 'B', number+10*Baxis_multiplier) #+ line.split(r'Z', 1)[1]
+        print "temp line: {}".format(temp_line)
+        line = temp_line
     if "E" in line:
         E_in = True
         (front, back) = line.split(r'E', 1)[0], line.split(r'E', 1)[1]
-        print "back is: {}".format(back)
+        
         if " F" in back:
             Fback = back.split(r'F', 1)[1]
             new_line = front + 'F' + Fback
@@ -75,7 +118,7 @@ for line in f3:
                 if "extrusion" in new_line:
                     check = 1
                 if check < 0.5:
-                    print "check is not 1"
+                    
                     f2.write("$DO{}.0 = 1\n".format(valve))
                 check = 0
                 
@@ -100,12 +143,13 @@ for line in f3:
         tool_status= -tool_status
         if tool_status == -1:
             f2.write('$DO{}.0 = 0\n'.format(valve))
+            f2.write('Call setPress P{} Q{}\n'.format(com_port, silver_pressure))
             f2.write('G91\n')
             f2.write('F40\n')
-            f2.write('G1 A10\n')
+            f2.write('G1 A10 \n')
             f2.write('$currentX = AXISSTATUS(X, DATAITEM_PositionFeedback)\n')
             f2.write('$currentY = AXISSTATUS(Y, DATAITEM_PositionFeedback)\n')
-            f2.write('G1 X{} Y{}\n'.format(-x_offset, -y_offset))
+            f2.write('G1 X{} Y{}\n'.format(x_offset, y_offset))
             f2.write('G92 X$currentX Y$currentY\n')
             f2.write('G1 B-10\n')
             f2.write('G90\n')
@@ -114,24 +158,31 @@ for line in f3:
             z_axis = 'B'
         elif tool_status ==1:
             f2.write('$DO{}.0 = 0\n'.format(valve))
+            f2.write('Call setPress P{} Q{}\n'.format(com_port, matrix_pressure))
             f2.write('G91\n')
             f2.write('F40\n')
             f2.write('G1 B10\n')
             f2.write('POSOFFSET CLEAR X Y\n')
-            f2.write('G1 X{} Y{}\n'.format(x_offset, y_offset))
-            f2.write('G90\n')
+            f2.write('G1 X{} Y{}\n'.format(-x_offset, -y_offset))
             f2.write('G1 A-10\n')
+            f2.write('G90\n')
             f2.write('G1 F{}\n'.format(matrix_feed))
             valve = 0
             z_axis = 'A'
-    print "new line pre file_write: {}  ident: {}".format(new_line, ident)
+            
+   
     if ident != 1:
         f2.write(new_line)
     E_in = False
     F_in = False
     ident = 2
-    #f2.write(new_line)        
-    print new_line
-    
+   
+
+f2.write('Call togglePress P{}'.format(com_port))
+
+stuff = footer.readlines()
+f2.writelines(stuff)    
 #f1.close()
 f2.close()
+header.close()
+footer.close()
