6fb73965e99792ef59a65a22f328e2f3909f43fb
Travis Busbee <travis@voxel8.co>
2014-12-14 19:16:04 -0500

    dynamic z height

--- Euclid_post_process.py
+++ Euclid_post_process.py
@@ -1,7 +1,7 @@
 
 ### Trav's Laptop#########
-f1 = open(r'C:\Users\Workstation 1\Documents\GitHub\Total_Epoxilation\test_wire-sliced.gcode', 'r')
-f3 = open(r'C:\Users\Workstation 1\Documents\GitHub\Total_Epoxilation\test_code_processed_2.gcode', 'w')
+f1 = open(r'C:\Users\Workstation 1\Documents\GitHub\Total_Epoxilation\0.55_root_planter_box_v4_solid (repaired)-sliced (2).gcode', 'r')
+f3 = open(r'C:\Users\Workstation 1\Documents\GitHub\Total_Epoxilation\test_code_processed_3.gcode', 'w')
 
 
 
@@ -13,6 +13,11 @@ ident = 0
 
 
 for line in f1:
+    if "Z" in line:
+        if "X" in line:
+            back_line = line.split("Z", 1)[1]
+            current_z_string = back_line.split(' ', 1)[0]
+    
     if ident == 4:
         if "Z" in line:
             if "X" in line:
@@ -46,11 +51,20 @@ for line in f1:
         f3.write(new_z + '\n')
         ident = 0
           
+    if ident ==6:
+        current_z_value = float(current_z_string)
+        safe_z = current_z_value + 3
+        f3.write('G1 Z' + str(safe_z) + ' F1000\n')  
+        ident = 0    
+          
     if ";T0 activated" in line:
         ident = 2
         
     if ";T1 activated" in line:
         ident = 4
+        
+    if ";move to z" in line:
+        ident = 6
     
 
 f1.close()
